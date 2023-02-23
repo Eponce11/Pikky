be81d947dd603600cf18e43a6d1ce147d1d5cea3
Eric Ponce <ponceeric458@gmail.com>
2023-02-22 22:52:35 -0800

    Login Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import {
 import Profile from './pages/profile'
 import Post from './components/post'
 import Follow from './components/follow'
+import Login from './pages/login'
 
 function App() {
 
@@ -19,9 +20,10 @@ function App() {
       <Router>
         <Routes>
 
-          <Route path='/' element={<Profile/>}/>
+          <Route path='/' element={<Login/>}/>
           <Route path='/post' element={<Post/>}/>
           <Route path='/followers' element={<Follow/>}/>
+          <Route path='/profile' element={<Profile/>}/>
 
         </Routes>
       </Router>
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,26 @@
+
+
+
+
+const Login = () => {
+
+
+    return (
+        <div className="flex justify-center items-center bg-[green] h-screen w-screen">
+            <div className="bg-[blue] ">
+                <div>
+                    <label>Email</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="text" />
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+
+export default Login
