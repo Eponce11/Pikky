11cdf81f1e7a4bd5425d68e66adaf0a3dcd8a917
Eric Ponce <ponceeric458@gmail.com>
2023-03-04 22:46:19 -0800

    api calls to login and register

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,20 +1,55 @@
 import { useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import axios from "axios";
 
 
+interface LoginData {
+    email: string;
+    password: string
+}
 
 const Login = () => {
 
+    const [formData, setFormData] = useState<LoginData>({
+        email: '',
+        password: ''
+    })
+    const [error, setError] = useState<string>('');
+
+    const { email, password } = formData;
+
     const navigate = useNavigate();
+
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>): void => {
+        e.preventDefault();
+        setFormData( (prevState:LoginData) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleLogin = (e:React.MouseEvent<HTMLElement>): void => {
+        e.preventDefault();
+        axios.post('http://127.0.0.1:5000/api/user/login', formData)
+            .then( (response) => {
+                console.log(response.data)
+                navigate('/home')
+            })
+            .catch( (error) => {
+                setError(error.response.data)
+            })
+    }
 
     return (
         <div className="flex justify-center items-center bg-[green] h-screen w-screen">
             <div className="bg-[blue] h-80 w-96">
                 <h1 className="text-[48px] text-center">Pikky</h1>
+                <span>{ error }</span>
                 <p>Email:</p>
-                <input type="text" className="mb-4" />
+                <input type="text" className="mb-4" name='email' value={email} onChange={onChange} />
                 <p>Password:</p>
-                <input type="text" />
-                <button className="bg-[red] w-full block" onClick={ () => { navigate("/home") } } >Login</button>
+                <input type="text" name='password' value={password} onChange={onChange} />
+                <button className="bg-[red] w-full block" onClick={handleLogin} >Login</button>
             </div>
         </div>
     )
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,12 +1,59 @@
+import { useNavigate } from "react-router-dom"
+import React, { useState } from "react"
+import axios from "axios"
+
+interface RegisterData {
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string
+}
 
 
 const Register = () => {
 
+    const [formData, setFormData] = useState<RegisterData>({
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
 
+    const { username, firstName, lastName, email, password, confirmPassword } = formData
+
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>): void => {
+        e.preventDefault();
+        setFormData( (prevState:RegisterData) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleRegister = (e:React.MouseEvent<HTMLElement>): void => {
+        e.preventDefault();
+        axios.post('http://127.0.0.1:5000/api/user/create', formData)
+            .then( (response) => {
+                console.log(response.data)
+            })
+            .catch( (error) => {
+                console.log(error.response.data)
+            })
+    }
 
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="bg-[red] p-10 aspect-square" />
+            <input type="text" name='username' value={username} onChange={onChange}/>
+            <input type="text" name='firstName' value={firstName} onChange={onChange}/>
+            <input type="text" name='lastName' value={lastName} onChange={onChange}/>
+            <input type="text" name='email' value={email} onChange={onChange}/>
+            <input type="text" name='password' value={password} onChange={onChange}/>
+            <input type="text" name='confirmPassword' value={confirmPassword} onChange={onChange}/>
+            <button>Register</button>
         </div>
     )
 }
