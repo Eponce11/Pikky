8522a975f39794f0fdb6a6bf33697852a4413192
Eric Ponce <ponceeric458@gmail.com>
2023-04-07 22:08:47 -0700

    persistant redux storage

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,14 +1,31 @@
 
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from "redux-persist"
+import thunk from 'redux-thunk'
+
+
 import signedInUserSlice from "../features/signedInUserSlice"
 import visitedSearchedUsersSlice from "../features/visitedSearchedUsersSlice"
 
 
+const reducers = combineReducers({
+    signedInUser: signedInUserSlice,
+    visitedSearchedUsers: visitedSearchedUsersSlice
+})
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = configureStore({
-    reducer: {
-        signedInUser: signedInUserSlice,
-        visitedSearchedUsers: visitedSearchedUsersSlice
-    }
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: [thunk]
 })
 
 export type RootState = ReturnType<typeof store.getState>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,18 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
+let persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <App />
+        </PersistGate>
     </Provider>
 )
