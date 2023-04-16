781716c8ba0c98600e784734bf1084cf3e134bbb
Eric Ponce <ponceeric458@gmail.com>
2023-04-15 20:30:52 -0700

    relationship routes

--- server/flask_app/controllers/controller_userrelationships.py
+++ server/flask_app/controllers/controller_userrelationships.py
@@ -8,8 +8,37 @@ from flask_app.models.model_userrelationship import UserRelationship
 @app.route('/api/userRelationship/new', methods=['POST'])
 def newUserRelationship():
     data = json.loads(request.data)
-    userRelationship = UserRelationship.newRelationship(data)
+    print(data)
+    id = UserRelationship.newRelationship(data)
 
     return {
         'Msg': "Followed"
     }
+
+@app.route('/api/userRelationship/getFollowers', methods=['POST'])
+def getUserFollowers():
+    data = json.loads(request.data)
+    
+    userFollowers = UserRelationship.getOneUserFollowers(data)
+
+    response = app.response_class(
+        response = json.dumps(userFollowers),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+    return response
+
+@app.route('/api/userRelationship/getFollowing', methods=['POST'])
+def getUserfollowing():
+    data = json.loads(request.data)
+    
+    userFollowing = UserRelationship.getOneUserFollowing(data)
+
+    response = app.response_class(
+        response = json.dumps(userFollowing),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    
+    return response
