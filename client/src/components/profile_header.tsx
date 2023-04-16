39d6570237006dbed18beaf70509d98827bf0296
Eric Ponce <ponceeric458@gmail.com>
2023-04-15 20:31:52 -0700

    get followers and following

--- client/src/components/profile_header.tsx
+++ client/src/components/profile_header.tsx
@@ -1,8 +1,9 @@
 
 import { useNavigate } from "react-router-dom";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useAppSelector } from "../app/hooks"
 import DefaultProfilePicture from "../static/icon-profile-pic.svg"
+import { getUserFollowers, getUserfollowing, newUserRelationship } from "../functions/api";
 
 interface BasicUserInfo {
     username: string,
@@ -12,13 +13,51 @@ interface BasicUserInfo {
 }
 
 const ProfileHeader = (props: BasicUserInfo) => {
+
     const signedInUserId = useAppSelector( (state) => state.signedInUser.id)
-    const { username, profilePicture, numOfPosts } = props
-
-    const [isFollowing] = useState(false); // add is following feature
-
+    const { username, profilePicture, numOfPosts, id } = props
+    const [followerCount, setFollowerCount] = useState<any>(0);
+    const [followingCount, setFollowingCount] = useState<any>(0);
+    const [isFollowing, setIsFollowing] = useState<any>(false);
     const navigate = useNavigate();
+    
+    useEffect( () => {
+        getUserFollowersCount();
+        getUserFollowingCount();
+    }, [])
 
+    const getUserFollowersCount = async (): Promise<void> => {
+        try {
+            const userFollowers = await getUserFollowers('1')
+            setFollowingCount(userFollowers.length)
+            userFollowers.map( (follower:any) => {
+                if (follower.follower_id === signedInUserId){
+                    setIsFollowing(true);
+                }
+            })
+        } catch (err:any) {
+            console.log(err)
+        }
+    }
+
+    const getUserFollowingCount = async (): Promise<void> => {
+        try {
+            const userFollowing = await getUserfollowing('1')
+            setFollowerCount(userFollowing.length)
+            console.log(userFollowing)
+        } catch (err:any) {
+            console.log(err)
+        }
+    }
+
+    const handleFollow = async (e:React.MouseEvent<HTMLElement>): Promise<void> => {
+        e.preventDefault();
+        try {
+            await newUserRelationship({ follower_id: signedInUserId, following_id: id})
+        } catch (err:any) {
+            console.log(err)
+        }
+    }
 
     return (
         <div className="text-[12px] pb-4 bg-primary-white">
@@ -36,11 +75,11 @@ const ProfileHeader = (props: BasicUserInfo) => {
                 <div className="h-12 col-span-3 pl-4">
                     <ul className="flex justify-center relative">
                         <li className="flex flex-col items-center absolute left-0 mt-2" onClick={ () => { navigate('/user/followers') }}>
-                            <p>2021</p>
+                            <p>{followerCount}</p>
                             <p>Followers</p>
                         </li>
                         <li className="flex flex-col items-center mt-2" onClick={ () => { navigate('/user/following') }}>
-                            <p>201</p>
+                            <p>{followingCount}</p>
                             <p>Following</p>
                         </li>
                         <li className="flex flex-col items-center absolute right-0 mt-2">
@@ -54,11 +93,11 @@ const ProfileHeader = (props: BasicUserInfo) => {
                 </div>
                 <div className="flex items-center h-9 col-span-3 pl-4">
                     {
-                        signedInUserId === props.id ?
+                        signedInUserId === id ?
                             <button className="primary-btn h-8 px-6 py-1" onClick={ () => { navigate(`/edit/${signedInUserId}`) }}>Edit Profile</button> :
-                            isFollowing ?
-                                <button className="secondary-btn h-8 w-full ">Unfollow</button> :
-                                <button className="secondary-btn h-8 px-6 py-1 ">Follow</button>
+                                isFollowing ?
+                                    <button className="secondary-btn h-8 w-full ">Unfollow</button> :
+                                    <button className="secondary-btn h-8 px-6 py-1" onClick={handleFollow}>Follow</button>
                     }
                 </div>
             </div>
